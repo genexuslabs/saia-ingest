effe3150cc71e7b97402ace2da672093660fef5e
Luciano Silveira <ladrians@gmail.com>
2024-01-06 07:42:38 -0300

    add file detection strategy

--- saia_ingest/utils.py
+++ saia_ingest/utils.py
@@ -1,3 +1,6 @@
+import os
+import magic
+import logging
 import yaml
 
 def get_yaml_config(yaml_file):
@@ -7,4 +10,25 @@ def get_yaml_config(yaml_file):
     return config
 
 
+def detect_file_extension(file_path):
+    mime = magic.Magic()
+
+    # Open the file in binary mode
+    with open(file_path, 'rb') as file:
+        file_signature = file.read(1024)
+    # Detect the file type based on the magic number
+    file_type = mime.from_buffer(file_signature)
+    file_type = file_type.lower()
+
+    if 'text' in file_type:
+        return '.txt'
+    elif 'microsoft word' in file_type:
+        return '.docx'
+    elif 'image' in file_type:
+        return '.png'  # Add more image types as needed
+    elif 'pdf' in file_type:
+        return '.pdf'
+    else:
+        logging.getLogger().info(f"{file_path} unknown file type: {file_type}")
+        return ""
 
